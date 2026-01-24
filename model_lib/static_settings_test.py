fba18d4871a6719bfcf92cb55233c685150aa671
Espen Albert <espen.albert@mongodb.com>
2026-01-24 18:28:24 +0000

    chore: fix type hint errors

--- model_lib/static_settings_test.py
+++ model_lib/static_settings_test.py
@@ -18,8 +18,8 @@ def test_static_settings_for_testing(tmp_path):
     settings = StaticSettings.for_testing(tmp_path=tmp_path)
     assert settings.STATIC_DIR == tmp_path / "static"
     assert settings.CACHE_DIR == tmp_path / "cache"
-    assert settings.STATIC_DIR.exists()
-    assert settings.CACHE_DIR.exists()
+    assert settings.STATIC_DIR and settings.STATIC_DIR.exists()
+    assert settings.CACHE_DIR and settings.CACHE_DIR.exists()
 
 
 def test_static_settings_app_name():
@@ -28,8 +28,8 @@ def test_static_settings_app_name():
 
 def test_static_settings_roots(tmp_path):
     settings = StaticSettings.for_testing(tmp_path=tmp_path)
-    assert settings.static_root == settings.STATIC_DIR / "static"
-    assert settings.cache_root == settings.CACHE_DIR / "static"
+    assert settings.STATIC_DIR and settings.static_root == settings.STATIC_DIR / "static"
+    assert settings.CACHE_DIR and settings.cache_root == settings.CACHE_DIR / "static"
 
 
 def test_static_settings_skip_app_name(tmp_path):
